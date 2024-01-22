de17b0596fd376bdda53e74290bb5a32d4375db6
kenkenm43 <kenkenm43@gmail.com>
2024-01-22 23:56:18 +0700

    first commit

---
+++ src-no-context/components/Progress.js
@@ -0,0 +1,18 @@
+function Progress({ index, numQuestion, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress max={numQuestion} value={index + Number(answer !== null)} />
+
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestion}
+      </p>
+      <p>
+        <strong>
+          {points} / {maxPossiblePoints}
+        </strong>
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
